fad3ba4bda87dd1ca36595c544a26e612460e5d8
Margaux Armanet <112400605+margauxarmanet@users.noreply.github.com>
2023-03-05 01:00:46 +0100

    Update product.js
    - add product to cart (localStorage).
    - limit per product to 100.
    - update product quantity in localStorage.

--- front/js/product.js
+++ front/js/product.js
@@ -23,6 +23,55 @@ async function fetchProduct() {
 
   let productQuantity = document.querySelector("#quantity");
   productQuantity.value = 1;
+
+  // Ajoute le produit au panier.
+  const AddToCart = document.querySelector("#addToCart");
+  AddToCart.addEventListener("click", (event) => {
+    event.preventDefault();
+    const selectedProduct = {
+      id: productId,
+      imageUrl,
+      altTxt,
+      name,
+      color: productColor.value,
+      quantity: Number(productQuantity.value),
+      price,
+    };
+
+    // On vérifie que la clé "products" existe, sinon on l'a crée.
+    let keyProducts = JSON.parse(localStorage.getItem("products"));
+
+    if (keyProducts === null) {
+      keyProducts = [];
+      keyProducts.push(selectedProduct);
+      localStorage.setItem("products", JSON.stringify(keyProducts));
+    } // Si la clé "products" existe, on vérifie que selectedProduct existe.
+    else {
+      let productInStorage = keyProducts.find((product) => {
+        return (
+          product.id == selectedProduct.id &&
+          product.color == selectedProduct.color
+        );
+      });
+      // Si selectedProduct existe dans la clé "products", on additionne les quantités.
+      if (productInStorage) {
+        const totalQuantity =
+          Number(productInStorage.quantity) + Number(selectedProduct.quantity);
+        if (totalQuantity <= 100) {
+          productInStorage.quantity = totalQuantity;
+        } else {
+          alert(
+            `Vous pouvez commander au maximum 100 ${name} de couleur ${productColor.value}.`
+          );
+        }
+      } // Si selectedProduct n'existe pas dans la clé "products", on le crée.
+      else {
+        keyProducts.push(selectedProduct);
+      }
+      localStorage.setItem("products", JSON.stringify(keyProducts));
+    }
+    window.location.href = "cart.html";
+  });
 }
 
 fetchProduct();
