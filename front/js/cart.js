36a44cc5e6d9f7fa78b2e19971f9f9484611b8f4
Margaux Armanet <112400605+margauxarmanet@users.noreply.github.com>
2023-03-05 22:31:34 +0100

    Create cart.js
    - displays the products on the page.
    - delete button works.
    - quantity button works.

---
+++ front/js/cart.js
@@ -0,0 +1,83 @@
+const keyProducts = JSON.parse(localStorage.getItem("products"));
+const cart = document.querySelector("#cart__items");
+
+// affiche les produits dans le panier.
+if (keyProducts) {
+  const productsInCart = keyProducts
+    .map(
+      (product) =>
+        `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+            <div class="cart__item__img">
+                <img src="${product.imageUrl}" alt="${product.alTxt}">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                <h2>${product.name}</h2>
+                <p>${product.color}</p>
+                <p>${product.price} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                    <p>Qté : </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
+                </div>
+                </div>
+            </div>
+        </article>`
+    )
+    .join("");
+
+  cart.innerHTML = productsInCart;
+} else {
+  cart.innerHTML = `<p style="text-align: center">Votre panier Kanap est vide.</p>`;
+}
+
+// gestion des produits dans le panier.
+if (keyProducts) {
+  const quantity = document.querySelectorAll(".itemQuantity");
+  const deleteItem = document.querySelectorAll(".deleteItem");
+
+  // met à jour des quantités :
+  quantity.forEach((input) => {
+    input.addEventListener("change", (event) => {
+      event.preventDefault();
+
+      const article = input.closest(".cart__item");
+      const { id, color } = article.dataset;
+      let productInStorage = keyProducts.find((inStorage) => {
+        return inStorage.id == id && inStorage.color == color;
+      });
+
+      if (input.value <= 100) {
+        productInStorage.quantity = input.value;
+      } else {
+        alert(
+          `Vous pouvez commander au maximum 100 ${productInStorage.name} de couleur ${productInStorage.color}.`
+        );
+      }
+
+      localStorage.setItem("products", JSON.stringify(keyProducts));
+    });
+  });
+
+  // supprime le produit :
+  deleteItem.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const article = deleteButton.closest(".cart__item");
+      const { id, color } = article.dataset;
+      let productInStorage = keyProducts.find((inStorage) => {
+        return inStorage.id == id && inStorage.color == color;
+      });
+
+      keyProducts.splice(productInStorage, 1);
+      localStorage.setItem("products", JSON.stringify(keyProducts));
+      article.remove();
+    });
+  });
+}
+// gestion du formulaire.
