022a5163f50441f5f84c95e294d608b1837111ac
Margaux Armanet <112400605+margauxarmanet@users.noreply.github.com>
2023-03-11 00:03:43 +0100

    update.js
    - prepared product.js and script.js for my openClassroom presentation.

--- front/js/product.js
+++ front/js/product.js
@@ -1,77 +1,156 @@
+// Objectif (1): Récupérer les données transmises par l'api grâce à son id.
+// Objectif (2): Afficher les données sur la page.
+// Objectif (3): Ajouter le produit et ses options dans le localStorage.
+// Objectif (4): Gestions des erreurs.
+
+// (1) Je récupére les données transmises par l'api grâce à son id.
 const productId = new URLSearchParams(window.location.search).get("id");
-const productUrl = `http://localhost:3000/api/products/${productId}`;
 
 async function fetchProduct() {
-  const response = await fetch(productUrl);
-  const { imageUrl, altTxt, name, price, description, colors } =
-    await response.json();
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/products/${productId}`
+    );
+    const product = await response.json();
 
-  // Affiche le produit sur la page.
-  document.title = name;
+    // (2) J'affiche le produit sur la page.
+    displayProduct(product);
 
-  const productImage = document.querySelector(".item__img");
-  productImage.innerHTML = `<img src="${imageUrl}" alt="${altTxt}">`;
-
-  document.querySelector("#title").textContent = name;
-  document.querySelector("#price").textContent = price;
-  document.querySelector("#description").textContent = description;
-
-  const productColor = document.querySelector("#colors");
-  productColor.innerHTML = colors
-    .map((color) => `<option value="${color}">${color}</option>`)
-    .join("");
-
-  let productQuantity = document.querySelector("#quantity");
-  productQuantity.value = 1;
-
-  // Ajoute le produit au panier.
-  const AddToCart = document.querySelector("#addToCart");
-  AddToCart.addEventListener("click", (event) => {
-    event.preventDefault();
-    const selectedProduct = {
-      id: productId,
-      imageUrl,
-      altTxt,
-      name,
-      color: productColor.value,
-      quantity: Number(productQuantity.value),
-      price,
-    };
-
-    // On vérifie que la clé "products" existe, sinon on l'a crée.
-    let keyProducts = JSON.parse(localStorage.getItem("products"));
-
-    if (keyProducts === null) {
-      keyProducts = [];
-      keyProducts.push(selectedProduct);
-      localStorage.setItem("products", JSON.stringify(keyProducts));
-    } // Si la clé "products" existe, on vérifie que selectedProduct existe.
-    else {
-      let productInStorage = keyProducts.find((product) => {
-        return (
-          product.id == selectedProduct.id &&
-          product.color == selectedProduct.color
-        );
-      });
-      // Si selectedProduct existe dans la clé "products", on additionne les quantités.
-      if (productInStorage) {
-        const totalQuantity =
-          Number(productInStorage.quantity) + Number(selectedProduct.quantity);
-        if (totalQuantity <= 100) {
-          productInStorage.quantity = totalQuantity;
-        } else {
-          alert(
-            `Vous pouvez commander au maximum 100 ${name} de couleur ${productColor.value}.`
-          );
-        }
-      } // Si selectedProduct n'existe pas dans la clé "products", on le crée.
-      else {
-        keyProducts.push(selectedProduct);
-      }
-      localStorage.setItem("products", JSON.stringify(keyProducts));
-    }
-    window.location.href = "cart.html";
-  });
+    // (3) Au clic, j'ajoute le produit dans le localStorage.
+    document
+      .querySelector("#addToCart")
+      .addEventListener("click", () => addToCart(product));
+  } catch (error) {
+    // (4) Gestion des erreurs.
+    document.querySelector(".item").innerHTML =
+      "<p>Une erreur s'est produite lors du chargement de la page.</p>";
+    console.error(error);
+  }
 }
 
 fetchProduct();
+
+// -------------------------------------------------------------------
+// Fonctions :
+
+// (2) J'affiche le produit sur la page.
+function displayProduct(product) {
+  document.title = product.name;
+
+  document.querySelector(
+    ".item__img"
+  ).innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+
+  document.querySelector("#title").textContent = product.name;
+  document.querySelector("#price").textContent = product.price;
+  document.querySelector("#description").textContent = product.description;
+
+  document.querySelector("#colors").innerHTML = product.colors
+    .map((color) => `<option value="${color}">${color}</option>`)
+    .join("");
+
+  document.querySelector("#quantity").value = 1;
+}
+
+// (3) Au clic, j'ajoute le produit dans le localStorage.
+function addToCart(product) {
+  const selectedProduct = createSelectedProduct(product);
+
+  let keyProducts = initializeKeyProducts();
+
+  // Je vérifie si selectedProduct est dans keyProducts.
+  let productInStorage = findProductInStorage(selectedProduct, keyProducts);
+  if (productInStorage) {
+    // selectedProduct n'est pas dans keyProducts: j'ajoute selectedProduct.
+    updateQuantity(selectedProduct, productInStorage);
+  } else {
+    // selectedProduct est dans keyProducts: je modifie selectedProduct.
+    addSelectedProduct(selectedProduct, keyProducts);
+  }
+  localStorage.setItem("products", JSON.stringify(keyProducts));
+}
+
+// -------------------------------------------------------------------
+// "Sous-Fonctions" de (3) :
+
+// Je crée l'objet qui doit être envoyé dans le localStorage.
+function createSelectedProduct(product) {
+  const productColor = document.querySelector("#colors").value;
+  const productQuantity = document.querySelector("#quantity").value;
+
+  const selectedProduct = {
+    id: productId,
+    imageUrl: product.imageUrl,
+    altTxt: product.altTxt,
+    name: product.name,
+    color: productColor,
+    quantity: Number(productQuantity),
+    price: product.price,
+  };
+
+  return selectedProduct;
+}
+
+// J'initialise keyProducts :
+function initializeKeyProducts() {
+  let keyProducts = JSON.parse(localStorage.getItem("products"));
+
+  if (keyProducts === null) {
+    keyProducts = [];
+    localStorage.setItem("products", JSON.stringify(keyProducts));
+  }
+
+  return keyProducts;
+}
+
+// Je vérifie si selectedProduct est dans keyProducts.
+function findProductInStorage(selectedProduct, keyProducts) {
+  let productInStorage = keyProducts.find((product) => {
+    return (
+      product.id == selectedProduct.id && product.color == selectedProduct.color
+    );
+  });
+  return productInStorage;
+}
+
+// selectedProduct n'est pas dans keyProducts: j'ajoute selectedProduct.
+function addSelectedProduct(selectedProduct, keyProducts) {
+  if (selectedProduct.quantity > 100) {
+    alert(
+      `Vous pouvez commander au maximum 100 ${selectedProduct.name} de cette couleur.`
+    );
+  } else {
+    keyProducts.push(selectedProduct);
+    window.location.href = "cart.html";
+  }
+}
+
+// selectedProduct est dans keyProducts: je modifie selectedProduct.
+function updateQuantity(selectedProduct, productInStorage) {
+  const newQuantity =
+    Number(selectedProduct.quantity) + Number(productInStorage.quantity);
+  if (newQuantity > 100) {
+    alert(
+      `Vous pouvez commander au maximum 100 ${selectedProduct.name} de cette couleur.`
+    );
+  } else {
+    productInStorage.quantity = newQuantity;
+    window.location.href = "cart.html";
+  }
+}
+
+// -------------------------------------------------------------------
+// Explication URLSearchParams :
+/* new URLSearchParams(window.location.search).get("id"):
+
+- window.location récupère l'url de la page actuelle : 
+  "http://127.0.0.1:5500/front/html/product.html?id=107fb5b75607497b96722bda5b504926"
+
+- window.loccation.search : récupère uniquement la query string de l'url :
+  "?id=107fb5b75607497b96722bda5b504926"
+
+URLSearchParams est une classe js qui permet de manipuler les paramètres de la query string d'une URL.
+- new URLSearchParams(window.location.search) : crée un nouvel objet avec le contenu de la query string en paramètre.
+
+- .get("id") : récupère le paramètre "id" de notre query string:
+  "107fb5b75607497b96722bda5b504926" */
--- front/js/script.js
+++ front/js/script.js
@@ -1,41 +1,52 @@
-const productsUrl = "http://localhost:3000/api/products";
+// Objectif (1): Récupérer les données transmises par l'api.
+// Objectif (2): Afficher les données sur la page.
+// Objectif (3): Gestions des erreurs.
 
-// Récupère les produits.
+// (1) Je récupère les données des produits.
 async function fetchProducts() {
-  const response = await fetch(productsUrl);
-  const data = await response.json();
+  try {
+    const response = await fetch("http://localhost:3000/api/products");
+    const products = await response.json();
 
-  // Affiche les produits sur la page.
-  const productsContainer = document.querySelector("#items");
-  const productCard = data
-    .map(
-      (product) =>
-        `<a href="./product.html?id=${product._id}">
-            <article>
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-                <h3 class="productName">${product.name}</h3>
-                <p class="productDescription">${product.description}</p>
-            </article>
-        </a>`
-    )
-    .join("");
+    // (2) J'affiche les produits sur la page.
+    const productCard = products
+      .map(
+        (product) =>
+          `<a href="./product.html?id=${product._id}">
+              <article>
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                  <h3 class="productName">${product.name}</h3>
+                  <p class="productDescription">${product.description}</p>
+              </article>
+          </a>`
+      )
+      .join("");
 
-  productsContainer.innerHTML = productCard;
+    document.querySelector("#items").innerHTML = productCard;
+  } catch (error) {
+    // (3) Gestion des erreurs.
+    document.querySelector("#items").innerHTML =
+      "<p>Une erreur s'est produite lors du chargement des produits.</p>";
+    console.error(error);
+  }
 }
 
 fetchProducts();
 
+// -------------------------------------------------------------------
+// async function :
+/* J'ai choisi d'utiliser une fonction asynchrone :
+- pour faciliter la lecture et la maintenance du code.
+- pour faciliter la gestion de potentielles erreurs avec try/catch. */
+
+// products.map(fonction).join(""):
+/* J'ai choisi d'utiliser les méthodes .map() et .join() :
+- pour faciliter la lecture et la maintenance du code.
+- pour réduire la quantité de code à écrire. */
+
 /* 
-Explications pour la présentation :
-data.map(fonction).join("");
-
-data[] est l'array qui contient les produits qu'on a récupéré.
-.map(fonction qui se répéte pour chaque objet contenu dans l'array data).
-.map() est le nouvel array qui reçoit le code généré par la fonction.
-
-l'array .map() contient le code généré par la fonction comme ceci : 
-["string html du produit 0", "string html du produit 1", etc].
-
-.join("") indique qu'on ne veut pas de séparateur "," entre chaque string de code html.
-
+.map(fonction qui se répète sur chaque objet de products) est le nouvel
+array qui contient le code généré par la fontion.
+.join("") indique que je ne veux pas de séparateur "," entre chaque string
+de code html contenue dans l'array .map().
 */
